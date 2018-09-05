31ec350e3dabdea543c74fd374bbc71c27a6b092
pandemoniumMX <promartinez2@gmail.com>
2018-09-05 17:51:49 -0600

    Estructura Pdf Generar orden, listo:v

--- pdf-orden.php
+++ pdf-orden.php
@@ -66,19 +66,80 @@
     $pdf->SetFont('Arial','B',12);
     $pdf->SetXY(145,95);
     $pdf->Write(5,'MODELO');
-  //falla
+  //Número de serie
   $pdf->SetFont('Arial','',12);
   $pdf->SetXY(17,110);
-  $pdf->Write(5,'Con la falla(s) de:');
+  $pdf->Write(5,'Numero de serie:');
 
   $pdf->SetFont('Arial','B',12);
   $pdf->SetXY(53,110);
+  $pdf->Write(5,'#SERIE');
+
+//Falla 
+  $pdf->SetFont('Arial','',12);
+  $pdf->SetXY(17,125);
+  $pdf->Write(5,'Con la(s) falla(s) de:');
+
+  $pdf->SetFont('Arial','B',12);
+  $pdf->SetXY(60,125);
   $pdf->Write(5,'FALLA');
+//servicio y accesorios.
+  $pdf->SetFont('Arial','',12);
+  $pdf->SetXY(17,140);
+  $pdf->Write(5,'Con el servicio de:');
+
+  $pdf->SetFont('Arial','B',12);
+  $pdf->SetXY(55,140);
+  $pdf->Write(5,'SERVICIO');
+
+  $pdf->SetFont('Arial','',12);
+  $pdf->SetXY(100,140);
+  $pdf->Write(5,'accesorios que deja:');
+
+  $pdf->SetFont('Arial','B',12);
+  $pdf->SetXY(145,140);
+  $pdf->Write(5,'ACCESORIOS');
+
+//politicas 
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(17,170);
+$pdf->Write(6,'No nos hacemos responsables por equipos olvidados, tanto como los que estan reparados y los
+que no, en caso de ser un servicio a domicilio, no se le cobrara por la revision solo si reparamos
+su equipo, en caso de que no desee una reparacion, se le cobrara $200.00 por el traslado');
+
+//footer :v
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(20,210);
+$pdf->Write(6,'Sin mas por el momento queda de ustedes su seguro servidor Juan Jorge Diaz Ochoa.');
+
+//direccion
+$pdf->SetFont('Arial','B',12);
+$pdf->SetXY(90,225);
+$pdf->Write(6,'Direccion');
+
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(45,235);
+$pdf->Write(6,'Prolongacion Leona Vicario esquina con Lagunas de Montebello.');
+
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(55,245);
+$pdf->Write(6,'Col. Lagunitas, Cabo San Lucas, Baja California Sur');
+
+//contacto
+$pdf->SetFont('Arial','B',12);
+$pdf->SetXY(90,255);
+$pdf->Write(6,'Contacto');
+
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(80,265);
+$pdf->Write(6,'Cel: 624-176-33-02');
+
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(80,270);
+$pdf->Write(6,'Oficina: 688-28-96');
 
 
 
 
-
-
-    $pdf->Output();
+  $pdf->Output();
 ?>
