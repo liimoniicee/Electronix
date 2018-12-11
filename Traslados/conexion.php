d6f4af582aec36820616ab3c00a85af1647ab59d
pandemoniumMX <promartinez2@gmail.com>
2018-12-11 14:16:29 -0700

    Encriptacion Fase 1

--- Traslados/conexion.php
+++ Traslados/conexion.php
@@ -1,23 +1,15 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "electronicax";
-
-// Crear connection
-$conn = @mysqli_connect($servername, $username, $password, $dbname);
-return $conn;
-// revisar connection
-
 /*
-$servername = "fdb23.runhosting.com";
-$username = "2877282_electronicax";
-$password = "clubtvs2018";
-$dbname = "2877282_electronicax";
-$port ="3306";
-// Crear connection
-$conn = @mysqli_connect($servername, $username, $password, $dbname,$port);
-return $conn;
-*/
+<?php
+// Este archivo es protegido por la ley del derechos de propiedad literaria. La ingenieria inversa de este codigo se prohibe estrictamente.
+eval(gzinflate(base64_decode('
+tZJNboMwEIX3SNzBQl2kURTAyGCJplTqQSLjnwYJ
+bDoG2t6+NknahKTL7ux5M+95Prl6Dp6q/tCHQRg8
+WAmTBM06iXYoag1n7cHYISqdNtpfBYw5Fntm7YcB
+4YuUFLWgBcF5moo8JZQpmqgsIVjxQhE8D4j67CFb
+yQcwuuHs00thEMeXfp0gq+umx1MXegXJAHGjtVMb
+o52vv7ihl+7LvrfN/qStLjbaoJ8V3PEc5I7HJ3lz
+kMMIGs1m5ZwEcmrsIssJ6xtWStQ428KoPa9Gv225
+6W6wYVoUmOL9cvUrjLwd62GyOEnpgtjf4wYGtIuy
+LMmj8t8IbeacO5zWcRhU/ht9Aw==
+')));
 ?>
--- conexion.php
+++ conexion.php
@@ -1,25 +1,15 @@
 <?php
+// Este archivo es protegido por la ley del derechos de propiedad literaria. La ingenieria inversa de este codigo se prohibe estrictamente.
+eval(gzinflate(base64_decode('
+tZJNboMwEIX3SNzBQl2kURTAyGCJplTqQSLjnwYJ
+bDoG2t6+NknahKTL7ux5M+95Prl6Dp6q/tCHQRg8
+WAmTBM06iXYoag1n7cHYISqdNtpfBYw5Fntm7YcB
+4YuUFLWgBcF5moo8JZQpmqgsIVjxQhE8D4j67CFb
+yQcwuuHs00thEMeXfp0gq+umx1MXegXJAHGjtVMb
+o52vv7ihl+7LvrfN/qStLjbaoJ8V3PEc5I7HJ3lz
+kMMIGs1m5ZwEcmrsIssJ6xtWStQ428KoPa9Gv225
+6W6wYVoUmOL9cvUrjLwd62GyOEnpgtjf4wYGtIuy
+LMmj8t8IbeacO5zWcRhU/ht9Aw==
+')));
 
-$servername = "localhost";
-$username = "root";
-$password = "857bd8752611d6158af80f3052fc7f52";
-$dbname = "electronicax";
-
-//$password = md5("electronicax");
-
-// Crear connection
-$conn = @mysqli_connect($servername, $username, $password, $dbname);
-return $conn;
-// revisar connection
-
-/*
-$servername = "fdb23.runhosting.com";
-$username = "2877282_electronicax";
-$password = "clubtvs2018";
-$dbname = "2877282_electronicax";
-$port ="3306";
-// Crear connection
-$conn = @mysqli_connect($servername, $username, $password, $dbname,$port);
-return $conn;
-*/
 ?>
